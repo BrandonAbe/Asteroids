13445ff0c545cd419ffdca46ff681c5633ea7791
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-04-19 17:18:54 -0400

    Create game screen window

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Imports
 import pygame
 from constants import *
-
+import sys
 
 def main():
     print("Starting Asteroids!")
@@ -10,6 +10,17 @@ def main():
 
     # Initialize the pygame
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Create game loop
+    while True:
+        screen.fill('black')
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
 if __name__ == "__main__":
     main()
