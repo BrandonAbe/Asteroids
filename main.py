e3e471a74d8328a1da3b09931ce4b0a50c063645
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-04-19 17:22:22 -0400

    add lock to 60fps

--- main.py
+++ main.py
@@ -11,16 +11,21 @@ def main():
     # Initialize the pygame
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 # Delta time
 
     # Create game loop
     while True:
-        screen.fill('black')
-        pygame.display.flip()
-
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: # Check if the event is a quit event
                 pygame.quit()
                 sys.exit()
+
+        screen.fill('black')
+        pygame.display.flip()
+        clock.tick(FPS)
+        dt = clock.get_time() / 1000.0
+
 
 if __name__ == "__main__":
     main()
