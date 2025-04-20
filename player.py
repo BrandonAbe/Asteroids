a6e6d8c853d24f4e1bdf0ae3811f0388c65db893
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-04-20 10:54:10 -0400

    update init variables

--- player.py
+++ player.py
@@ -1,8 +1,15 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.velocity = pygame.Vector2(0, 0)
         self.rotation = 0
+
+    def draw(self,screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
