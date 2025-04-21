217ac5e3e8f6319ccdd42ba783ece982f179e410
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-04-21 18:09:29 -0400

    replaced asteroid.kill() with asteroid.split

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def main():
             for shot in shots:
                 if asteroid.collision_detection(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill('black')
 
